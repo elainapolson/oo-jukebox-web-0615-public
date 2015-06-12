d5abe46f3df94f2023c8fd88ca2bb0746c8eab87
elainapolson <elainapolson@gmail.com>
2015-06-12 12:57:16 -0400

    Done

---
+++ lib/jukebox.rb
@@ -0,0 +1,55 @@
+class Jukebox
+
+  def initialize(songs)
+    @songs = songs
+  end
+
+  def call
+    help
+    list
+    play
+  end
+
+  def help
+    puts "(?=.*help)(?=.*list)(?=.*play)(?=.*exit)"
+  end
+
+  def list
+    @songs.each.with_index(1) do |song, index|
+      puts "#{index}. #{song}"
+    end 
+  end 
+
+
+  def play(song = "")
+    if song == ""
+      puts "Now Playing: Phoenix - 1901"
+    elsif @songs.include?(song)
+      puts "Now Playing: #{song}"
+    else
+      puts "Now Playing: #{@songs[song.to_i-1]}"
+    end  
+  end 
+
+  def exit_jukebox
+    puts "Goodbye"
+  end
+
+
+
+  # def run(songs)
+  #   puts "Please enter a command:"
+  #   command = gets.strip.downcase
+    
+  #     case command
+  #     when "help"
+  #       help
+  #     when "list"
+  #       list(songs)
+  #     when "play"
+  #       play(songs)
+  #     when "exit"    
+  #       exit_jukebox
+  #     end
+  # end
+end
